405bc0cceb93947e9e23fbfa337b817dc60913be
koty08 <koty08@naver.com>
2026-02-05 14:52:46 +0900

    🔧 turbopack polling 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
+  watchOptions: {
+    pollIntervalMs: 1000,
   },
 };
 
