0f1d9bc4a363bbf58953548112193db70988f996
koty08 <koty08@naver.com>
2026-03-07 17:40:53 +0900

    ✨ 권한 확인을 위한 API Route 용 HOC 함수 제작

--- src/lib/auth-server.ts
+++ src/lib/auth-server.ts
@@ -1,5 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
 import { auth } from "./auth";
 import { headers } from "next/headers";
+import { apiError } from "@/consts/apiError";
 
 export const checkIsKoty = async () => {
   "use server";
@@ -9,7 +11,18 @@ export const checkIsKoty = async () => {
 
   if (!session) return false;
   else {
-    console.log(`${process.env.ADMIN_KOTY_EMAIL}`);
     return session.user.email === `${process.env.ADMIN_KOTY_EMAIL}` ? true : false;
   }
+};
+
+type RouteHandler = (request: NextRequest) => Promise<NextResponse>;
+
+export const checkIsKotyWrapper = (handler: RouteHandler) => {
+  return async (request: NextRequest) => {
+    const isKoty = await checkIsKoty();
+    if (!isKoty) return apiError.unauthorized;
+
+    const response = await handler(request);
+    return response;
+  };
 };
